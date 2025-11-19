85c10bbeb25d957232c2b9a6a41ec1017fcd1e26
Baptiste <baptistev59@free.fr>
2025-11-19 17:45:49 +0100

    Ajout de l'import MigratorConfiguration dans setupController

--- src/Controller/SetupController.php
+++ src/Controller/SetupController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Migrations\MigratorConfiguration;
 
 class SetupController extends AbstractController
 {
