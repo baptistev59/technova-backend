8049cebf0510c889d16df75bdd5ff6610e4e0490
Baptiste VANDAELE <baptistev59@free.fr>
2026-01-31 13:08:15 +0100

    Fix déploiement: charger WebpackEncoreBundle seulement en dev/test (évite l'erreur ClassNotFound en prod)

--- config/bundles.php
+++ config/bundles.php
@@ -21,5 +21,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['dev' => true, 'test' => true],
 ];
